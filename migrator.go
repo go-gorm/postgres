8e3232380638bd0053468567c37b76cd5b96a148
Cr <631807682@qq.com>
2022-04-07 23:36:47 +0800

    fix: AutoMigrate panic
    commit 17752a9d043b3638517227535d396d9462d85173
    Author: Jinzhu <wosmvp@gmail.com>
    Date:   Thu Apr 7 23:35:41 2022 +0800
    
        Refactor #97
    
    commit 28f957e3378a29bd61ee93eaaafa4cc073da9871
    Author: a631807682 <631807682@qq.com>
    Date:   Thu Apr 7 18:03:19 2022 +0800
    
        fix: AutoMigrate panic

--- migrator.go
+++ migrator.go
@@ -238,11 +238,11 @@ func (m Migrator) AlterColumn(value interface{}, field string) error {
 		if field := stmt.Schema.LookUpField(field); field != nil {
 			var (
 				columnTypes, _  = m.DB.Migrator().ColumnTypes(value)
-				fieldColumnType migrator.ColumnType
+				fieldColumnType *migrator.ColumnType
 			)
 			for _, columnType := range columnTypes {
 				if columnType.Name() == field.DBName {
-					fieldColumnType, _ = columnType.(migrator.ColumnType)
+					fieldColumnType, _ = columnType.(*migrator.ColumnType)
 				}
 			}
 
