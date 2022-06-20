351278493e0d258b87f289816ccf3105cbf804fb
zhangjie <zhangjie@test.com>
2022-06-20 11:15:02 +0800

    feat: add postgres index

--- migrator.go
+++ migrator.go
@@ -586,3 +586,107 @@ func (m Migrator) getColumnSequenceName(tx *gorm.DB, stmt *gorm.Statement, field
 	)
 	return
 }
+
+func (m Migrator) GetIndexes(value interface{}) ([]gorm.Index, error) {
+	indexes := make([]gorm.Index, 0)
+
+	indexs := make([]struct {
+		TableName string `gorm:"column:tablename"`
+		IndexName string `gorm:"column:indexname"`
+		IndexDef  string `gorm:"column:indexdef"`
+	}, 0, 5)
+
+	err := m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		indexSql := "select tablename,indexname,tablespace,indexdef from pg_indexes where tablename=?"
+		scanErr := m.DB.Raw(indexSql, m).Scan(&indexs).Error
+		if scanErr != nil {
+			return scanErr
+		}
+		result := make([]*Index, 0)
+
+		reg := regexp.MustCompile(`\([^\(\)]*\)`) // reg get the index columns
+		if reg == nil {
+			return fmt.Errorf("regexp err")
+		}
+		for _, index := range indexs {
+
+			// get index fields
+			columns := reg.FindAllString(index.IndexDef, -1)
+			if len(columns) < 1 {
+				return fmt.Errorf("columns is zero")
+			}
+			// def eg: CREATE INDEX "user.i_user_id_name" ON public.t_user USING btree (f_name, f_user_id)
+			columns[0] = strings.TrimFunc(columns[0], func(r rune) bool {
+				if string(r) == `(` || string(r) == `)` {
+					return true
+				}
+				return false
+			})
+
+			for _, s := range strings.Split(columns[0] /*get the first */, ",") {
+				ind := &Index{}
+				ind.IndexName = index.IndexName
+				ind.TableName = index.TableName
+				ind.NonUnique = 1
+				if strings.Contains(index.IndexDef, "CREATE UNIQUE") {
+					ind.NonUnique = 0
+				}
+				ind.ColumnName = strings.TrimSpace(s)
+				//ind.SeqInIndex = int32(i + 1)
+				result = append(result, ind)
+			}
+
+		}
+		if len(result) == 0 {
+			return nil
+		}
+
+		indexMap := groupByIndexName(result)
+
+		for _, idx := range indexMap {
+			if len(idx) == 0 {
+				continue
+			}
+			tempIdx := &migrator.Index{
+				TableName: idx[0].TableName,
+				NameValue: idx[0].IndexName,
+				PrimaryKeyValue: sql.NullBool{
+					Bool:  idx[0].IndexName == idx[0].TableName+"_pkey",
+					Valid: true,
+				},
+				UniqueValue: sql.NullBool{
+					Bool:  idx[0].NonUnique == 0,
+					Valid: true,
+				},
+			}
+			for _, x := range idx {
+				tempIdx.ColumnList = append(tempIdx.ColumnList, x.ColumnName)
+			}
+			indexes = append(indexes, tempIdx)
+		}
+		return nil
+	})
+	return indexes, err
+}
+
+// Index table index info
+type Index struct {
+	TableName  string `gorm:"column:TABLE_NAME"`
+	ColumnName string `gorm:"column:COLUMN_NAME"`
+	IndexName  string `gorm:"column:INDEX_NAME"`
+	NonUnique  int32  `gorm:"column:NON_UNIQUE"`
+}
+
+func groupByIndexName(indexList []*Index) map[string][]*Index {
+	columnIndexMap := make(map[string][]*Index, len(indexList))
+	if len(indexList) == 0 {
+		return columnIndexMap
+	}
+	for _, idx := range indexList {
+		if idx == nil {
+			continue
+		}
+		columnIndexMap[idx.IndexName] = append(columnIndexMap[idx.IndexName], idx)
+	}
+	return columnIndexMap
+}
