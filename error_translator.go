5de971423231b8e6d3eec654155a611ea1dfe9a0
Muhammad Amir Ejaz <37077032+codingamir@users.noreply.github.com>
2023-05-18 15:19:04 +0500

    added 23503 pgerrcode support and updated the error conversion approach

--- error_translator.go
+++ error_translator.go
@@ -5,14 +5,16 @@ import (
 	"gorm.io/gorm"
 )
 
-var errCodes = map[string]string{
-	"uniqueConstraint": "23505",
+var pgerrodesToGormErrors = map[string]error{
+	"23505": gorm.ErrDuplicatedKey,
+	"23503": gorm.ErrForeignKeyViolated,
 }
 
 func (dialector Dialector) Translate(err error) error {
 	if pgErr, ok := err.(*pgconn.PgError); ok {
-		if pgErr.Code == errCodes["uniqueConstraint"] {
-			return gorm.ErrDuplicatedKey
+		val, ok := pgerrodesToGormErrors[pgErr.Code]
+		if ok {
+			return val
 		}
 	}
 
