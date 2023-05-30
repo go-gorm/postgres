5d0099409098411b22615c8ca032bcc1fe784762
Saeid Saeidee <s.saeidee@sensysgatso.com>
2023-05-30 11:20:34 +0200

    feat: foreign key violated error

--- error_translator.go
+++ error_translator.go
@@ -8,8 +8,9 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
-var errCodes = map[string]string{
-	"uniqueConstraint": "23505",
+var errCodes = map[string]error{
+	"23505": gorm.ErrDuplicatedKey,
+	"23503": gorm.ErrForeignKeyViolated,
 }
 
 type ErrMessage struct {
@@ -22,8 +23,8 @@ type ErrMessage struct {
 // Since currently gorm supporting both pgx and pg drivers, only checking for pgx PgError types is not enough for translating errors, so we have additional error json marshal fallback.
 func (dialector Dialector) Translate(err error) error {
 	if pgErr, ok := err.(*pgconn.PgError); ok {
-		if pgErr.Code == errCodes["uniqueConstraint"] {
-			return gorm.ErrDuplicatedKey
+		if translatedErr, found := errCodes[pgErr.Code]; found {
+			return translatedErr
 		}
 		return err
 	}
@@ -39,8 +40,8 @@ func (dialector Dialector) Translate(err error) error {
 		return err
 	}
 
-	if errMsg.Code == errCodes["uniqueConstraint"] {
-		return gorm.ErrDuplicatedKey
+	if translatedErr, found := errCodes[errMsg.Code]; found {
+		return translatedErr
 	}
 	return err
 }
