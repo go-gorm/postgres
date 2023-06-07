18bc84b75196b6cb41f64edc59f2676acabe089f
Saeid <sk.saeidee@yahoo.com>
2023-06-07 09:04:28 +0200

    feat: foreign key violated error (#193)
    * feat: foreign key violated error
    
    * ci: fixed ci workflow to be triggered for all pull requests
    
    * test: fixed typo
    
    ---------
    
    Co-authored-by: Saeid Saeidee <s.saeidee@sensysgatso.com>

---
+++ error_translator_test.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"errors"
+	"github.com/jackc/pgx/v5/pgconn"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestDialector_Translate(t *testing.T) {
+	type fields struct {
+		Config *Config
+	}
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   error
+	}{
+		{
+			name: "it should return ErrDuplicatedKey error if the status code is 23505",
+			args: args{err: &pgconn.PgError{Code: "23505"}},
+			want: gorm.ErrDuplicatedKey,
+		},
+		{
+			name: "it should return ErrForeignKeyViolated error if the status code is 23503",
+			args: args{err: &pgconn.PgError{Code: "23503"}},
+			want: gorm.ErrForeignKeyViolated,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dialector := Dialector{
+				Config: tt.fields.Config,
+			}
+			if err := dialector.Translate(tt.args.err); !errors.Is(err, tt.want) {
+				t.Errorf("Translate() expected error = %v, got error %v", err, tt.want)
+			}
+		})
+	}
+}
